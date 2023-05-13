92013a3316f43a4675a04be54cf97163a2996f50
unknown <arsham.joolaei8@gmail.com>
2023-05-13 11:21:27 +0330

    Chenge error message

--- src/pages/Validation/Validation.jsx
+++ src/pages/Validation/Validation.jsx
@@ -7,7 +7,7 @@ import useCountdown from "../../components/UseCountdown";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import ValidationValidation from "./ValidationValidation.jsx";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 
 function Validation() {
   const { t } = useTranslation();
@@ -20,11 +20,7 @@ function Validation() {
 
   const navigate = useNavigate();
 
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
   let [minutes, seconds, setCountDown] = useCountdown(180);
-
-  const [validationCode, setValidationCode] = useState("");
 
   let sendCodeAgain = () => {
     setCountDown(180);
@@ -40,51 +36,39 @@ function Validation() {
       <Row className="justify-content-center">
         <Col md="5">
           <Card className="center-block">
-            {useMemo(
-              () => (
-                <Card.Header as="h6">
-                  <BsShieldLock className="m-2" />
-                  {t("Validation Code")}
-                </Card.Header>
-              ),
-              [isSubmitted]
-            )}
+            <Card.Header as="h6">
+              <BsShieldLock className="m-2" />
+              {t("Validation Code")}
+            </Card.Header>
+
             <Card.Body>
               <Card.Text>{t("Insert Validation Code")}</Card.Text>
               <Form onSubmit={handleSubmit(() => onSubmit())}>
-                {useMemo(
-                  () => (
-                    <InputGroup className="mb-2">
-                      <Form.Label column sm={3}>
-                        {t("Validation Code")}
-                      </Form.Label>
+                <InputGroup className="mb-2">
+                  <Form.Label column sm={3}>
+                    {t("Validation Code")}
+                  </Form.Label>
 
-                      <Col sm={9}>
-                        <InputGroup className="mb-3">
-                          <InputGroup.Text id="basic-addon1">
-                            <BsShieldLock />
-                          </InputGroup.Text>
-                          <Form.Control
-                            {...register("validation_code")}
-                            isInvalid={errors.validation_code}
-                            placeholder={t("Validation Code")}
-                            aria-label={t("Validation Code")}
-                            aria-describedby="basic-addon1"
-                            onChange={(event) => {
-                              setValidationCode(event.target.value);
-                              console.log(validationCode);
-                            }}
-                          />
-                          <Form.Control.Feedback type="invalid">
-                            {errors.validation_code &&
-                              errors.validation_code.message}
-                          </Form.Control.Feedback>
-                        </InputGroup>
-                      </Col>
+                  <Col sm={9}>
+                    <InputGroup className="mb-3">
+                      <InputGroup.Text id="basic-addon1">
+                        <BsShieldLock />
+                      </InputGroup.Text>
+                      <Form.Control
+                        {...register("validation_code")}
+                        isInvalid={errors.validation_code}
+                        placeholder={t("Validation Code")}
+                        aria-label={t("Validation Code")}
+                        aria-describedby="basic-addon1"
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.validation_code &&
+                          errors.validation_code.message}
+                      </Form.Control.Feedback>
                     </InputGroup>
-                  ),
-                  [validationCode]
-                )}
+                  </Col>
+                </InputGroup>
+
                 {useMemo(
                   () => (
                     <InputGroup className="justify-content-end mb-3">
@@ -95,34 +79,26 @@ function Validation() {
                   ),
                   [minutes, seconds]
                 )}
-                {useMemo(
-                  () => (
-                    <InputGroup className="justify-content-end">
-                      <div className="justify-content-end">
-                        <Button
-                          disabled={!(seconds === "00" && minutes === "00")}
-                          onClick={sendCodeAgain}
-                          variant="primary"
-                        >
-                          {t("Send Validation Code Again")}
-                        </Button>
-                      </div>
-                      <div
-                        className="justify-content-start"
-                        style={{ marginRight: 20 }}
-                      >
-                        <Button
-                          variant="primary"
-                          type="submit"
-                          onClick={() => setIsSubmitted((prev) => !prev)}
-                        >
-                          {t("Accept")}
-                        </Button>
-                      </div>
-                    </InputGroup>
-                  ),
-                  [isSubmitted]
-                )}
+
+                <InputGroup className="justify-content-end">
+                  <div className="justify-content-end">
+                    <Button
+                      disabled={!(seconds === "00" && minutes === "00")}
+                      onClick={sendCodeAgain}
+                      variant="primary"
+                    >
+                      {t("Send Validation Code Again")}
+                    </Button>
+                  </div>
+                  <div
+                    className="justify-content-start"
+                    style={{ marginRight: 20 }}
+                  >
+                    <Button variant="primary" type="submit">
+                      {t("Accept")}
+                    </Button>
+                  </div>
+                </InputGroup>
               </Form>
             </Card.Body>
           </Card>
